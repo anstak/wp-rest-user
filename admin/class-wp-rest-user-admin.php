efeb6b180e33fa40ff9a6f4269b133824e4f549b
Jack Song <133342301@163.com>
2018-05-14 02:46:06 +1000

    Added Menu

--- admin/class-wp-rest-user-admin.php
+++ admin/class-wp-rest-user-admin.php
@@ -106,7 +106,7 @@ class Wp_Rest_User_Admin {
 			'REST User',
 			'manage_options',
 			$this->plugin_name,
-			array('partials/wp-rest-user-admin-display.php', 'admin_page')
+			array(plugin_dir_url(__FILE__) . 'partials/wp-rest-user-admin-display.php', 'admin_page')
 		);
 
 	}
