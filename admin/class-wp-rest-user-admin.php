71d6096c9d0b439d5e1d3c8a4355bf5e0c282c11
Jack Song <133342301@163.com>
2018-05-14 02:36:51 +1000

    Added Menu

--- admin/class-wp-rest-user-admin.php
+++ admin/class-wp-rest-user-admin.php
@@ -102,13 +102,6 @@ class Wp_Rest_User_Admin {
 	 */
 	public function add_settings_menu() {
 		add_options_page(
-			'My Options',
-			'My Plugin',
-			'manage_options',
-			'my-plugin.php',
-			'wporg_options_page_html'
-		);
-		add_options_page(
 			'My Options 2',
 			'My Plugin 2',
 			'manage_options',
@@ -132,45 +125,6 @@ class Wp_Rest_User_Admin {
 		}
 		echo '<p> welcome to our plugin page </p>';
 	}
-
-/**
- * top level menu:
- * callback functions
- */
-	function wporg_options_page_html() {
-		// check user capabilities
-		if (!current_user_can('manage_options')) {
-			return;
-		}
-
-		// add error/update messages
-
-		// check if the user have submitted the settings
-		// wordpress will add the "settings-updated" $_GET parameter to the url
-		if (isset($_GET['settings-updated'])) {
-			// add settings saved message with the class of "updated"
-			add_settings_error('wporg_messages', 'wporg_message', __('Settings Saved', 'wporg'), 'updated');
-		}
-
-		// show error/update messages
-		settings_errors('wporg_messages');
-		?>
- <div class="wrap">
- <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
- <form action="options.php" method="post">
- <?php
-// output security fields for the registered setting "wporg"
-		settings_fields('wporg');
-		// output setting sections and their fields
-		// (sections are registered for "wporg", each field is registered to a specific section)
-		do_settings_sections('wporg');
-		// output save settings button
-		submit_button('Save Settings');
-		?>
- </form>
- </div>
- <?php
-}
 
 	/**
 	 * Register the stylesheets for the admin area.
