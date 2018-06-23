c4f16a4dadd231e01e1fd1d6a060f8d075384d3b
Jack Song <133342301@163.com>
2018-06-24 02:36:46 +1000

    Updated version number

--- wp-rest-user.php
+++ wp-rest-user.php
@@ -16,7 +16,7 @@
  * Plugin Name:       WP REST User
  * Plugin URI:        https://sk8.tech
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.2.1
+ * Version:           1.3.0
  * Author:            SK8Tech
  * Author URI:        https://sk8.tech
  * License:           GPL-2.0+
